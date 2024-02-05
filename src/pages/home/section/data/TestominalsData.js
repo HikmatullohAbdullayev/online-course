c7986b19eab033b642b5db6b0ae68b81dab4f133
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2024-02-05 19:47:05 +0500

    `sadda

--- src/pages/home/section/data/TestominalsData.js
+++ src/pages/home/section/data/TestominalsData.js
@@ -10,19 +10,19 @@ const testominalsData = [
     },
     {
         id:"2",
-        img:"../../../../../src/assets/img/jason.png",
+        img:{Sarah},
         name:"Jason M",
         content:"The UI/UX design course exceeded my expectations. The instructor's expertise and practical assignments helped me improve my design skills. I feel more confident in my career now. Thank you!",
     },
     {
         id:"3",
-        img:"../../../../../src/assets/img/lilya.png",
+        img:{Sarah},
         name:"Emily R",
         content:"The web design course provided a solid foundation for me. The instructors were knowledgeable and supportive, and the interactive learning environment was engaging. I highly recommend it!",
     },
     {
         id:"4",
-        img:"../../../../../src/assets/img/michel.png",
+        img:{Sarah},
         name:"Michael K",
         content:"The UI/UX design course exceeded my expectations. The instructor's expertise and practical assignments helped me improve my design skills. I feel more confident in my career now. Thank you!",
     },
