13452ccb16c9e489eef9e2527179d4ac6a422fcd
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2024-02-06 20:24:08 +0500

    course card done

--- src/pages/home/section/components/BenefitCard.jsx
+++ src/pages/home/section/components/BenefitCard.jsx
@@ -1,28 +1,31 @@
-import React from 'react';
-import VectorTopIcon from "../../../../assets/icon/VectorTopIcon"
-import  cardData  from '../data/BenefitData.js';
-// table1080:grid table1080:grid-cols-2  mobil670:grid mobil670:
+import React from "react";
+import VectorTopIcon from "../../../../assets/icon/VectorTopIcon";
+import cardData from "../data/BenefitData.js";
 
 function BenefitCard(props) {
-    return (
-        <div className='grid   gap-10 mt-80 p-14 grid-cols-[repeat(auto-fill,minmax(330px,1fr))]    '>
-      
-            {
-                cardData?.map((item) =>(
-                    <div className="card p-30 bg-white rounded-6  hover:shadow-md" key={item.id}>
-                        <p className='flex justify-end font-600  p-10 text-number80 table1170:text-number70'>{item.number}</p>
-                        <h3 className='font-500 text-subTitle24 mb-14  p-6  table1170:text-name20'>{item.title}</h3>
-                        <p className='font-400 text-text18 mb-50 p-6 text-text table1170:text-text16 mobile3:mb-20'>{item.content}</p>
-                        <span className='flex justify-end p-  '><VectorTopIcon/></span>
-                    </div>
-                ))
-            }
-            
+  return (
+    <div className="grid   gap-10 mt-80 p-14 grid-cols-[repeat(auto-fill,minmax(330px,1fr))]    ">
+      {cardData?.map((item) => (
+        <div
+          className="card p-30 bg-white rounded-6  hover:shadow-md"
+          key={item.id}
+        >
+          <p className="flex justify-end font-600  p-10 text-number80 table1170:text-number70">
+            {item.number}
+          </p>
+          <h3 className="font-500 text-subTitle24 mb-14  p-6  table1170:text-name20">
+            {item.title}
+          </h3>
+          <p className="font-400 text-text18 mb-50 p-6 text-text table1170:text-text16 mobile3:mb-20">
+            {item.content}
+          </p>
+          <span className="flex justify-end p-  ">
+            <VectorTopIcon />
+          </span>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export default BenefitCard;
-
-
-// grid-cols-[repeat(auto-fill, minmax(250px, 1fr))]
--- src/pages/home/section/components/SectionTop.jsx
+++ src/pages/home/section/components/SectionTop.jsx
@@ -3,13 +3,16 @@ import React from 'react';
 function SectionTop(props) {
     return (
         <>
-       <div className="flex justify-between flex-col ">
-            <div className=" w-full px-20 flex justify-between items-center mb-6">
+       <div className="flex justify-between flex-col mobil2:flex mobil2:flex-col mobil2:items-start ">
+            <div className=" w-full px-20 flex justify-between items-center mb-6  ">
                 <h1 className='font-600 text-title48 mb-6 table:text-title36 mobile530:text-subTitle28'>{props.title}</h1>
-                <p className='py-18 px-24 bg-white rounded-6 shadow-sm cursor-pointer font-500 text-text18 hover:bg-primary hover:text-white table:py-14 table:px-18 tablemin:py-8 tablemin:px16 tablemin:text-text16 mobile530:text-text14'>View All</p>
+                <p className='py-18 px-24 bg-white rounded-6 shadow-sm cursor-pointer font-500 text-text18 hover:bg-primary hover:text-white table:py-14 table:px-18 tablemin:py-8 tablemin:px16 tablemin:text-text16 mobile530:text-text14 mobil2:hidden'>View All</p>
              
             </div>
-          <div className="max-w-[1000px] w-full p-20 ">  <p className='font-400 text-text18 text-text w-full mobile530:text-text14'>Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac cum eget habitasse in velit fringilla feugiat senectus in.</p></div>
+          <div className="max-w-[1000px] w-full p-20 ">  <p className='font-400 text-text18 text-text w-full mobile530:text-text14'>Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac cum eget habitasse in velit fringilla feugiat senectus in.</p>
+          
+          </div>
+          <p className=' hidden py-18 px-24 ml-20 bg-white rounded-6 shadow-sm cursor-pointer font-500 text-text18 hover:bg-primary hover:text-white table:py-14 table:px-18 tablemin:py-8 tablemin:px16 tablemin:text-text16 mobile530:text-text14 mobil2:block'>View All</p>
         </div>
         </>
     );
